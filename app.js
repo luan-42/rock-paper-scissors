cbda054681ffd4d64e469afe2ea1652f1fd1ffbc
Luan Ferreira <178341285+luan-42@users.noreply.github.com>
2024-10-26 13:03:05 -0300

    :sparkles: Adicionar interface de usuário
    - Utilizar o DOM para construir a interface.
    - Remover funções antigas.
    - Atualizar à página.
    - Adicionar imagens para a opção de pedra, papel e tesoura.
    - Adicionar estilo para a página.

--- app.js
+++ app.js
@@ -1,58 +1,65 @@
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    function getComputerChoice() {
-        const choice = Math.floor(Math.random() * 3);
-        switch (choice) {
-            case 0:
-                return "rock";
-            case 1:
-                return "paper";
-            case 2:
-                return "scissors";
-        }
-    }
-    
-    function getHumanChoice() {
-        let choice;
-        const options = ["rock", "paper", "scissors"];
-        do {
-            choice = window.prompt("rock, paper or scissors?", "");
-            if (choice !== null) {
-                choice = choice.toLowerCase();
-            }
-        } while (!options.includes(choice))
-        return choice;
-    }
-    
-    function playRound(humanChoice, computerChoice) {
-        console.log(`User: ${humanChoice}`);
-        console.log(`Computer: ${computerChoice}`);
+let humanScore = 0;
+let computerScore = 0;
 
-        switch (`${humanChoice} ${computerChoice}`) {
-            case "rock scissors":
-            case "paper rock":
-            case "scissors paper":
-                humanScore++;
-                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-                break;
-            case "scissors rock":
-            case "rock paper":
-            case "paper scissors":
-                computerScore++;
-                console.log(`You lose! ${humanChoice} loses ${computerChoice}.`);
-                break;
-            default:
-                console.log(`Draw! ${humanChoice} equals ${computerChoice}.`);
-        }
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
     }
-    
-    for (let i = 0; i < 5; i++) {
-        console.log(`${"-".repeat(20)} Round ${i + 1} ${"-".repeat(20)}`);
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    console.log(`${"-".repeat(20)} Score ${"-".repeat(20)}`);
-    console.log(`User: ${humanScore}`);
-    console.log(`Computer: ${computerScore}`);
 }
+
+const result = document.querySelector("#result");
+const round = document.querySelector("#round");
+const user = document.querySelector("#humanScore");
+const computer = document.querySelector("#computerScore");
+
+function newGame() {
+    humanScore = 0;
+    computerScore = 0;
+
+    result.textContent = "Rock, paper, scissors? You choose!";
+    round.textContent = "First to 5 points wins!";
+    user.innerHTML = `<b>User: </b>${humanScore}`;
+    computer.innerHTML = `<b>Computer: </b>${computerScore}`;
+}
+
+function playRound(humanChoice, computerChoice) {
+    switch (`${humanChoice} ${computerChoice}`) {
+        case "rock scissors":
+        case "paper rock":
+        case "scissors paper":
+            humanScore++;
+            result.textContent = "You win!";
+            round.textContent = `${humanChoice} beats ${computerChoice}.`
+            break;
+        case "scissors rock":
+        case "rock paper":
+        case "paper scissors":
+            computerScore++;
+            result.textContent = "You lose!";
+            round.textContent = `${humanChoice} loses ${computerChoice}.`;
+            break;
+        default:
+            result.textContent = "Draw!";
+            round.textContent = `${humanChoice} equals ${computerChoice}.`
+    }
+    user.innerHTML = `<b>User: </b>${humanScore}`;
+    computer.innerHTML = `<b>Computer: </b>${computerScore}`;
+}
+
+const options = document.querySelector("#options");
+options.addEventListener("click", (e) => {
+    playRound(e.target.alt, getComputerChoice());
+    
+    if (humanScore == 5 || computerScore == 5) {
+        window.alert(`${humanScore == 5 ? "You" : "Computer"} wins! Start again?`);
+        newGame();
+    };
+})
+
+newGame();
